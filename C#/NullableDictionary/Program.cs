d12c9b50e19b109e6595f279978ad81a997d36a3
rendya2501 <rendya2501@gmail.com>
2020-12-22 23:07:22 +0900

    20201222

---
+++ C#/NullableDictionary/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace NullableDictionary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+    
+    class NullableDict<K, V> : IDictionary<K, V>
+    {
+
+    }
+}
