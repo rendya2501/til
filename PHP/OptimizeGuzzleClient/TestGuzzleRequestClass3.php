b87f3ad8b9f6c56ce25745181dc4b2d9f8aebbd4
rendya2501 <rendya2501@gmail.com>
2021-02-04 19:00:05 +0900

    追加検証

--- PHP/OptimizeGuzzleClient/TestGuzzleRequestClass3.php
+++ PHP/OptimizeGuzzleClient/TestGuzzleRequestClass3.php
@@ -140,9 +140,9 @@ function executeAPI($client, $request)
     return 'Test'; //$response;
 }
 
-try {
-    // callGetAPI();
 
+function Test()
+{
     $body = [
         'json' => [
             'linkage_vendor_id' => 'aa',
@@ -163,12 +163,7 @@ try {
         ['Content-Type' => 'application/json']
     );
     // クライアント生成
-    $client = new Client(
-        [
-            'http_errors' => false
-            // 'base_uri' => 'https://httpbin.org'
-        ]
-    );
+    $client = new Client(['http_errors' => false]);
     $response = $client->request(
         $request->getMethod(),
         $request->getUri(),
@@ -177,6 +172,27 @@ try {
             $body
         ]
     );
+    echo $response->getBody();
+}
+
+try {
+    // callGetAPI();
+    $params = 'linkage_plan_ids=0553274689010021,0553274689010022,0553274689010023,0553274689010024';
+
+    // echo http_build_query(['0553274689010021,0553274689010022,0553274689010023,0553274689010024'],'?',',');
+    $get_request = new Request(
+        'GET',
+        'https://httpbin.org/get' . '?' . $params,
+        ['Authorization' => 'Bearer ' . 121212]
+    );
+
+    var_dump(mb_strstr($get_request->getRequestTarget(), '?'));
+
+    echo json_decode($get_request->getBody(), true) ?? "a";
+    echo "\n";
+    // クライアント生成
+    $client = new Client(['http_errors' => false]);
+    $response = $client->send($get_request);
     echo $response->getBody();
 } catch (Exception $e) {
     echo json_encode($e);
