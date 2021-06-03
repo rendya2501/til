644f2c549d7c2420fa97efc0c0c03fd7d1f72869
rendya2501 <rendya2501@gmail.com>
2021-06-03 17:32:39 +0900

    ツール,0件に対応&整理

--- PHP/GORA_500エラーメンテナンスツール/maintenance_script.php
+++ PHP/GORA_500エラーメンテナンスツール/maintenance_script.php
@@ -1,25 +1,25 @@
 <?php
+
+// 1.エントリーチェック
+
 // 引数チェック
 if ($argc != 2) {
     print $argc;
     print "引数が不正です。\n";
     exit;
 }
-
 // 解析対象ファイル名取得
 $filename = $argv[1];
-// 検索結果出力ファイル名
-$dfilename = preg_replace("/\.log\.gz$/", "", $filename) . "_.php";
 
-//gzファイルオープン
-$zh = gzopen($filename, "rb");
-if ($zh == false) {
-    print "gzファイルのオープンに失敗しました。\n";
-    exit;
-}
 
+// 2.ログ解析
+// 観測行
 $row_cnt = 1;
 $hit_row_count_array = [];
+// 観測文字列
+$str = '';
+$result_array = [];
+// 観測判定フラグ
 $flag1 = false;
 $flag2 = false;
 $flag3 = false;
@@ -28,16 +28,22 @@ $flag5 = false;
 $flag6 = false;
 $flag7 = false;
 $flag8 = false;
-$str = '';
-$result_array = [];
 
+// gzファイルオープン
+$zh = gzopen($filename, "rb");
+if ($zh == false) {
+    print "指定ファイルのオープンに失敗しました。\n";
+    exit;
+}
+// 全行ループ
 while (gzeof($zh) == false) {
     // 1行取得
-    $line = "";
     $line = gzgets($zh);
     $row_cnt += 1;
     $hit_flag = false;
 
+    // 各文字列を観測したらフラグを上げていく。
+    // 途中で別の文字列を観測したらリセットする。
     if (!$flag1 && strstr($line, '500') != false) {
         $hit_flag = true;
         $flag1  = true;
@@ -69,10 +75,13 @@ while (gzeof($zh) == false) {
     if ($flag7 && strstr($line, 'POST') != false) {
         $hit_flag = true;
         $flag8  = true;
+        // このタイミングで観測行を取得する
         $hit_row_count_array[] = $row_cnt;
     }
     if ($flag8 && strstr($line, 'POST') == false) {
+        // 結果を取得し続けるためフラグを上げ続ける
         $hit_flag = true;
+        // 次のログが始まったら観測を終了して結果に格納する
         if (strstr($line, 'App\Services\WebCooperation\PlanCooperationGORAWorkerService->callCreatePlanAPI') != false) {
             $result_array[] = $str;
             $flag1 = false;
@@ -88,7 +97,7 @@ while (gzeof($zh) == false) {
         }
         $str .= $line;
     }
-
+    // 1回でもヒットしなかったら全てリセット
     if (!$hit_flag) {
         $flag1 = false;
         $flag2 = false;
@@ -100,10 +109,25 @@ while (gzeof($zh) == false) {
         $flag8 = false;
     }
 }
-
 //gzファイルクローズ
 gzclose($zh);
 
+
+// 3.結果発表
+print "\n";
+if (count($hit_row_count_array) == 0) {
+    print "1件も観測できませんでした。\n";
+    exit;
+} else {
+    foreach ($hit_row_count_array as $hit_row_count) {
+        print $hit_row_count . "行目からのパラメータを取得しました。\n";
+    }
+}
+
+
+// 4.結果を一時ファイルに出力
+// 検索結果出力ファイル名
+$dfilename = preg_replace("/\.log\.gz$/", "", $filename) . "_.php";
 // 出力ファイルオープン
 $fp = fopen($dfilename, "wb");
 if ($fp == false) {
@@ -113,7 +137,7 @@ if ($fp == false) {
 // 出力内容生成
 $php = '<?php' . "\n";
 foreach ($result_array as $key => $value) {
-    $php .= 'function getArray' . $key + 1 . '(){' . "\n";
+    $php .= 'function getArray' . ((int)$key + 1) . '(){' . "\n";
     $php .= '    return ' . $value . ';' . "\n";
     $php .= '}';
 }
@@ -122,21 +146,19 @@ fputs($fp, json_decode(json_encode($php), true));
 //出力ファイルクローズ
 fclose($fp);
 
-print "\n";
-foreach ($hit_row_count_array as $hit_row_count) {
-    print $hit_row_count . "行目からのパラメータを取得しました。\n";
-}
 
-
-// 一時的な関数ファイル作成
+// 5.一時関数ファイルから配列を生成
+// 指定ファイル読み込み
 require $dfilename;
+// 配列の生成
 $source_array = [];
 foreach ($result_array as $key => $value) {
-    $funcname = "getArray" . $key + 1;
+    $funcname = "getArray" . ((int)$key + 1);
     $source_array[] = $funcname();
 }
 
 
+// 6.SQL生成
 $result = '';
 foreach ($source_array as $key => $source) {
     // 基本情報取得
@@ -146,7 +168,7 @@ foreach ($source_array as $key => $source) {
     $web_link_code = $golf_code . '3';
     $linkage_plan_id = substr($source['plans'][0]['linkage_plan_id'], 0, 16);
 
-    $result .= '-- ' . $key + 1 . '番目のクエリ' . "\n";
+    $result .= '-- ' . ((int)$key + 1) . '番目のクエリ' . "\n";
     // UPDATE文
     $result .= "UPDATE TmOpenPlanTargetSite SET WebPlanID='" . $linkage_plan_id . "',LinkFlag=1 \n";
     $result .= "WHERE GolfCode=" . $golf_code . " AND PlanCode=" . $plan_code . " AND OpenPlanCode=" . $open_plan_code . " AND WebLinkCode=" . $web_link_code;
@@ -192,6 +214,8 @@ foreach ($source_array as $key => $source) {
     $result .= "\n\n";
 }
 
+
+// 7.生成したSQLを出力
 // 出力ファイルオープン
 $fp = fopen($filename . "_.sql", "wb");
 if ($fp == false) {
@@ -202,7 +226,11 @@ if ($fp == false) {
 fputs($fp, $result);
 // 出力ファイルクローズ
 fclose($fp);
-// 一時関数ファイル削除
+
+
+// 8.一時関数ファイル削除
 unlink($filename . "_.php");
 
+
+// 9.処理終了
 print "処理が完了しました。\n";
