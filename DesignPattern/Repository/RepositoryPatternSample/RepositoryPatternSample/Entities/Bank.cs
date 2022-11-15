fe96f515d6562b6f43987edff45aa9fcdd6a29ff
rendya <rendya2501@gmail.com>
2022-11-16 00:06:03 +0900

    リポジトリパターン実装＆整理

---
+++ DesignPattern/Repository/RepositoryPatternSample/RepositoryPatternSample/Entities/Bank.cs
@@ -0,0 +1,8 @@
+﻿namespace RepositoryPatternSample.Entities
+{
+    public class Bank
+    {
+        public int BankId { get; set; }
+        public string BankName { get; set; } = string.Empty;
+    }
+}
---
+++ DesignPattern/Repository/RepositoryPatternSample/RepositoryPatternSample/Entities/BankBranch.cs
@@ -0,0 +1,9 @@
+﻿namespace RepositoryPatternSample.Entities
+{
+    public class BankBranch
+    {
+        public int BankId { get; set; }
+        public int BranchId { get; set; }
+        public string BranchName { get; set; } = string.Empty;
+    }
+}
