620f3a941062b1c875bf8d39f6370e1d1219e427
rendya2501 <rendya2501@gmail.com>
2023-04-01 19:33:27 +0900

    修正

--- DB_Migration/DbUp/DBMigration/CreateBundle.bat
+++ DB_Migration/DbUp/DBMigration/CreateBundle.bat
@@ -1,6 +1,12 @@
 @echo off
 chcp 65001
 
-powershell.exe -ExecutionPolicy Bypass -File ".\src\CreateBundle.ps1"
+cd .\src
+rmdir /s /q bin
+rmdir /s /q obj
+dotnet build --no-incremental
 
+powershell.exe -ExecutionPolicy Bypass -File ".\CreateBundle.ps1"
+
+echo "press any keys"
 pause > nul
